cc9a48d58965971eb4082f4b01297cc4c98d04e3
Pedro Espin <muertenegrasanpedro@hotmail.com>
2023-11-28 11:35:20 +0100

    Ordenando packages y añadiendo Agoritmia básica

---
+++ Agoritmia/src/main/java/org/example/Ejercicios.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Suma de Dos Números:
+ * Escribir un algoritmo que tome dos números como entrada y devuelva su suma.
+ * <p>
+ * Multiplicación de Tres Números:
+ * Crear un algoritmo que multiplique tres números dados como entrada.
+ * <p>
+ * Número Mayor:
+ * Diseñar un algoritmo que determine el número más grande entre tres valores.
+ * <p>
+ * Contador de Pares:
+ * Desarrollar un algoritmo que cuente la cantidad de números pares en una lista dada.
+ * <p>
+ * Factorial de un Número:
+ * Implementar un algoritmo para calcular el factorial de un número entero dado.
+ * <p>
+ * Inversión de Cadena:
+ * Escribir un algoritmo que invierta una cadena de caracteres.
+ * <p>
+ * Números Primos:
+ * Crear un algoritmo que determine si un número dado es primo o no.
+ * <p>
+ * Ordenamiento de Lista:
+ * Implementar un algoritmo de ordenamiento (por ejemplo, burbuja o selección) para ordenar una lista de números.
+ * <p>
+ * Fibonacci:
+ * Desarrollar un algoritmo para generar los primeros N términos de la secuencia de Fibonacci.
+ * <p>
+ * Palíndromo:
+ * Crear un algoritmo que verifique si una palabra o frase dada es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+ */
+public class Ejercicios {
+    public static void main(String[] args) {
+        System.out.println("-------SUMA DE DOS NUMEROS-------");
+        int resultado = suma(5, 7);
+        System.out.println("resultado = " + resultado);
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    Scanner sc = new Scanner(System.in);
+        System.out.println("resultado = "+resultado);
+
